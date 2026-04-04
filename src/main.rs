d23653228a50a2ccc14457ba0f555ba604a80384
xosc <urlocaldumbass@proton.me>
2026-04-04 15:14:23 +1300

    cli args

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,34 @@ use azalea::pathfinder::debug::PathfinderDebugParticles;
 use azalea::pathfinder::execute::simulation::SimulationPathfinderExecutionPlugin;
 use azalea::swarm::prelude::*;
 use azalea::{ClientInformation, prelude::*};
+use clap::Parser;
+use tracing::{error, warn};
 
 #[tokio::main]
 async fn main() -> AppExit {
     thread::spawn(deadlock_detection_thread);
 
-    let join_address = "localhost:25565";
-    let accounts = vec!["lickbot".to_string()];
+    let args = Args::parse();
+    let join_address = args.address.clone();
 
     let mut builder = SwarmBuilder::new()
         .set_handler(handle)
         .set_swarm_handler(swarm_handle)
         .add_plugins(SimulationPathfinderExecutionPlugin);
 
-    for username in accounts {
-        let account = Account::offline(&username);
+    for username_or_email in &args.accounts {
+        let account = if username_or_email.contains('@') {
+            Account::microsoft(username_or_email).await.unwrap()
+        } else {
+            Account::offline(username_or_email)
+        };
+
         builder = builder.add_account_with_state(account, State::new());
     }
 
     builder
         .join_delay(Duration::from_millis(100))
+        .set_swarm_state(SwarmState { args })
         .start(join_address)
         .await
 }
@@ -38,15 +46,31 @@ fn deadlock_detection_thread() {
             continue;
         }
 
-        println!("{} deadlocks detected", deadlocks.len());
+        error!("{} deadlocks detected", deadlocks.len());
         for (i, threads) in deadlocks.iter().enumerate() {
-            println!("Deadlock #{i}");
+            error!("Deadlock #{i}");
             for t in threads {
-                println!("Thread Id {:#?}", t.thread_id());
-                println!("{:#?}", t.backtrace());
+                error!("Thread Id {:#?}", t.thread_id());
+                error!("{:#?}", t.backtrace());
             }
         }
     }
+}
+
+#[derive(Parser, Debug, Clone, Default)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, num_args = 1.., default_values = ["lickbot"])]
+    /// Usernames or emails of the accounts to use, space separated. If it contains an '@', it will be treated as a Microsoft account, otherwise it will be treated as an offline account.
+    accounts: Vec<String>,
+
+    #[arg(short = 'A', long, default_value = "localhost:25565")]
+    /// The address of the server to connect to.
+    address: String,
+
+    #[arg(short = 'P', long, default_value_t = false)]
+    /// Whether to show where the bot is pathfinding to by spamming the /particle command. Off by default.
+    pathfinder_debug_particles: bool,
 }
 
 #[derive(Clone, Component, Default)]
@@ -59,16 +83,21 @@ impl State {
 }
 
 #[derive(Clone, Default, Resource)]
-struct SwarmState {}
+struct SwarmState {
+    pub args: Args,
+}
 
 async fn handle(bot: Client, event: azalea::Event, state: State) {
+    let swarm_state = bot.resource::<SwarmState>();
+
+    #[allow(clippy::single_match)]
     match event {
         azalea::Event::Init => {
             bot.set_client_information(ClientInformation {
                 view_distance: 32,
                 ..Default::default()
             });
-            if true {
+            if swarm_state.args.pathfinder_debug_particles {
                 bot.ecs
                     .write()
                     .entity_mut(bot.entity)
@@ -80,7 +109,11 @@ async fn handle(bot: Client, event: azalea::Event, state: State) {
 }
 
 async fn swarm_handle(swarm: Swarm, event: SwarmEvent, state: SwarmState) {
-    match event {
+    #[allow(clippy::single_match)]
+    match &event {
+        SwarmEvent::Disconnect(account, _join_opts) => {
+            warn!("bot got kicked! {}", account.username());
+        }
         _ => {}
     }
 }
